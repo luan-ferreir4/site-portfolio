393e9b4e0a672cffcb3ec00698873c5f10708bf2
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-06-11 21:38:34 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/App.css';
+
+// import { useState } from 'react';
+
+const App = () => {
+  // const [count, setCount] = useState(6);
+
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
