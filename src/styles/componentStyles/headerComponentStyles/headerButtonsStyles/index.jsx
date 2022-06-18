cb3bab398efff92758273effa765083fcd0927d7
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-06-17 23:33:58 -0300

    style: header style

---
+++ src/styles/componentStyles/headerComponentStyles/headerButtonsStyles/index.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Menu = styled.button`
+  background: transparent;
+  border: none;
+  .icon {
+    width: 42px;
+  }
+`;
---
+++ src/styles/componentStyles/headerComponentStyles/index.jsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  position: absolute;
+  left: 0;
+  right: 0;
+
+  display: flex;
+  flex-direction: row;
+
+  width: 95vw;
+  height: 52px;
+  padding: 16px 20px;
+  margin: 15px auto;
+
+  border-radius: 25px;
+  background: var(--palette-one-white);
+
+  @media (min-width: 768px) {
+    width: 96vw;
+    height: 60px;
+    padding: 10px 20px;
+  }
+
+  @media (min-width: 1024px) {
+  }
+`;
+
+export const MobileContent = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    width: 78.36px;
+  }
+
+  @media (min-width: 680px) {
+    display: none;
+  }
+`;
+
+export const DesktopContent = styled.section`
+  display: none;
+  @media (min-width: 680px) {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    img {
+      width: 100px;
+    }
+  }
+`;
