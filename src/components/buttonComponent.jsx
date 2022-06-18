4da2bd4d8d0efadc2ee24a3474f8abfb347e3ce5
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-06-17 23:15:47 -0300

    feat: button component

---
+++ src/components/buttonComponent.jsx
@@ -0,0 +1,7 @@
+import { ButtonFr } from '../styles/componentStyles/buttonComponentStyle';
+
+const ButtonComponent = ({ content }) => {
+  return <ButtonFr>{content}</ButtonFr>;
+};
+
+export default ButtonComponent;
