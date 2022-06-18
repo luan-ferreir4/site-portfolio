bb3900e3466ab8717de3153fc1ac1c86692f4e43
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-06-17 23:18:37 -0300

    feat: added header component and background to landing page

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,9 +1,17 @@
+import react from '../assets/images/reactBG2.svg';
+import HeaderComponent from '../components/header/headerComponent';
+import { Background, Landing } from '../styles/pageStyles/landingPage';
+
 const LandingPage = () => {
   return (
     <>
-      <main>
+      <HeaderComponent />
+      <Landing>
         <h1>Hello landing page</h1>
-      </main>
+      </Landing>
+      <Background>
+        <img className={'reactBG'} src={react} alt='React' />
+      </Background>
     </>
   );
 };
