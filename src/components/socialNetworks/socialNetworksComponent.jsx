4c8a1ced01ded270166867a52b8386317353ec58
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-07-05 20:22:37 -0300

    feat(components): add component to list social networks

---
+++ src/components/socialNetworks/socialNetworksComponent.jsx
@@ -0,0 +1,12 @@
+import NetworksItem from './networksItemComponent';
+const SocialNetworks = () => {
+  return (
+    <div>
+      <NetworksItem />
+      <NetworksItem />
+      <NetworksItem />
+    </div>
+  );
+};
+
+export default SocialNetworks;
