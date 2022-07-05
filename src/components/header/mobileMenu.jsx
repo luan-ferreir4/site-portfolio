09b36afe99bbc90e783124a78e9b5d192c3817cd
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-07-05 18:42:12 -0300

    refactor: new mobile menu component

--- src/components/header/mobileMenu.jsx
+++ src/components/header/mobileMenu.jsx
@@ -1,10 +1,30 @@
-import menuIcon from '../../assets/images/menu.svg';
+import { slide as Smenu } from 'react-burger-menu';
+import { RiMenu5Fill } from 'react-icons/ri';
+
 import { Menu } from '../../styles/componentStyles/headerComponentStyles/headerButtonsStyles';
 
 const MobileMenu = () => {
+  const openMenu = () => {
+    alert('Funfa');
+  };
+
   return (
     <Menu>
-      <img className={'icon'} src={menuIcon} alt='Menu' />
+      <RiMenu5Fill className={'icon'} />
+      <Smenu>
+        <a id='home' className='menu-item' href='#'>
+          Home
+        </a>
+        <a id='about' className='menu-item' href='#'>
+          About
+        </a>
+        <a id='contact' className='menu-item' href='#'>
+          Contact
+        </a>
+        <a onClick={openMenu} className='menu-item--small' href='#'>
+          Settings
+        </a>
+      </Smenu>
     </Menu>
   );
 };
