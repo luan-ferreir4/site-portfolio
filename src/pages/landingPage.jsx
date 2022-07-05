e8b10dbeac4180c3b0ac8d5af2e410c6f9272592
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-07-05 19:16:05 -0300

    refactor(landingPage): removed unused import

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,3 +1,4 @@
+import python from '../assets/images/python-logo.svg';
 import react from '../assets/images/reactBG2.svg';
 import HeaderComponent from '../components/header/headerComponent';
 import { Background, Landing } from '../styles/pageStyles/landingPage';
@@ -7,10 +8,12 @@ const LandingPage = () => {
     <>
       <HeaderComponent />
       <Landing>
-        <h1>Hello landing page</h1>
+        <h1 className='landing__greeting'>{'< Olá >'}</h1>
+        <h1 className='landing__greeting'>{'< Bem vindo! >'}</h1>
       </Landing>
       <Background>
         <img className={'reactBG'} src={react} alt='React' />
+        <img className={'pythonBG'} src={python} alt='Python' />
       </Background>
     </>
   );
