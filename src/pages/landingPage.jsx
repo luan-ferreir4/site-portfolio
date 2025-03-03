564a0bf61e395adb88dfc2dc0098bb9ca9a8cbe9
luan-ferreir4 <ferreirasilva.luan13@gmail.com>
2025-03-03 11:30:15 -0300

    refactor: landing page structure

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,27 +1,29 @@
 import python from '../assets/images/python-logo.svg';
 import react from '../assets/images/reactBG2.svg';
 import HeaderComponent from '../components/header/headerComponent';
+import ContentSection from '../components/csection/contentSection';
 import { Background, Landing } from '../styles/pageStyles/landingPage';
 
 const LandingPage = () => {
   return (
     <>
-      <HeaderComponent />
       <Landing>
-        <div className='landingGreeting'>
+        <ContentSection className='landingGreeting'>
+
           <h1 className='landingGreeting__item'>{'< Olá >'}</h1>
           <h1 className='landingGreeting__item'>{'< Bem vindo! >'}</h1>
-        </div>
 
-        <div className='landingIntro'>
-          <h2 className='landingIntro__item'>Meu nome é Luan Ferreira</h2>
-          <h2 className='landingIntro__item'>Desenvolvedor Full stack</h2>
-        </div>
+          <h2 className='landingIntro__item'>Meu nome é Luan Ferreira_</h2>
+          <h2 className='landingIntro__item'>E sou um desenvolvedor full-stack_</h2>
+
+        </ContentSection>
       </Landing>
+      {/*
       <Background>
         <img className={'reactBG'} src={react} alt='React' />
         <img className={'pythonBG'} src={python} alt='Python' />
       </Background>
+      */}
     </>
   );
 };
