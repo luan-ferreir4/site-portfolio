849d93ad84d78066e51b93bae91b42f80c565752
Rasputin <ferreirasilva.luan13@gmail.com>
2025-04-01 15:51:02 -0300

    refactor: csection coponent style

--- src/styles/componentStyles/csectionComponentStyle/csectionStyle.jsx
+++ src/styles/componentStyles/csectionComponentStyle/csectionStyle.jsx
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 export const Csection = styled.section `
-    height: 900px;
-    width: 95%;
+    display: flex;
+    flex-direction: column;
 
+    height: 900px;
+    width: 100%;
+    
+    background: linear-gradient(to bottom, transparent 40%, var(--pallete-one-black) 88%);
+    
+    position: relative;
+    overflow: hidden;
 `
--- src/styles/pageStyles/landingPage/index.jsx
+++ src/styles/pageStyles/landingPage/index.jsx
@@ -4,20 +4,21 @@ export const Landing = styled.main`
   height: 100vh;
   padding-bottom: 10px;
 
-  position: relative;
-  overflow: hidden;
   
-  .sectionOne {
+  /*.sectionOne {
     display: flex;
     flex-direction: column;
 
     height: 900px;
     width: 100%;
-
+    
     background: linear-gradient(to bottom, transparent 40%, var(--pallete-one-black) 88%);
-  }
-
-
+    
+    position: relative;
+    overflow: hidden;
+  }*/
+  
+  
   .landingGreeting{
     display: flex;
     flex-direction: column;
