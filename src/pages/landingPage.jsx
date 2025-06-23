69b6f970f842895759de9a89d99f30924486de56
luan-ferreir4 <ferreirasilva.luan13@gmail.com>
2025-06-22 22:04:21 -0300

    feat: updated about me text

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -27,17 +27,22 @@ const LandingPage = () => {
           <img className='sectionTwo_bkg' src={python} alt='Python' />
 
           <h1 className='landingGreeting__item title'>{'>> Sobre mim'}</h1>
-          <p className='landingResume__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis  augue non urna mollis efficitur. Etiam hendrerit tempus consequat.  Mauris non condimentum nisi. Fusce sed sapien eget eros venenatis  ultrices sit amet non dui. Morbi vitae aliquet elit. Phasellus posuere  enim diam, at placerat velit gravida vel. In hac habitasse platea  dictumst. In scelerisque eu mi eu dictum.</p>
+          <p className='landingResume__text'>
+            I've been studying IT since high school, where it was integrated with web development. There I learned the principles of logic, programming, and databases using C, Java, and MySQL.
+            <br/><br/>
+            After that, I completed a 2,000-hour course in web development, with half of it focused on the front end, including HTML, CSS, JavaScript, and React, and the other half on the back end, utilizing PostgreSQL, Python, and libraries such as Flask and Django to create REST APIs. And all of that working with the agile methods of Scrum and Kanban with random teams to simulate a real-life development team.
+            <br/><br/>
+            In my current job, I assist the IT department of a food distribution company with SQL reports on the ERP Sankhya and use the IReport program to build some of them. I believe that I can aggregate with my versatile and adaptable personality, strong will to learn new technologies and ease and enjoyment of working in a group.</p>
         </ContentSection>
 
         <ContentSection className='sectionThree'>
-          <div className='projectContainer'> 
+          <div className='projectContainer'>
             <h1 className='projectContainer__item' >Data Projects</h1>
             <div className='projectContainer__slide'>
-                z
+              z
             </div>
           </div>
-          <div className='projectContainer'> 
+          <div className='projectContainer'>
             <h1 className='projectContainer__item title__right' >Web Projects</h1>
             <div className='projectContainer__slide'>
 
