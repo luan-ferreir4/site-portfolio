ca631dfd713cda65252d8d0ce20e29aa6a565512
luan-ferreir4 <ferreirasilva.luan13@gmail.com>
2026-01-19 22:35:18 -0300

    feat: mobile version version completed

--- src/styles/pageStyles/landingPage/index.jsx
+++ src/styles/pageStyles/landingPage/index.jsx
@@ -76,6 +76,9 @@ export const Landing = styled.main`
 
   
 /*--------------Section Two----------------------*/
+  .sectionTwo{
+   height: 1050px;
+  }
   .landingResume__text{
     margin: 8% 6vw;
     font-size: var(--font-size-small);
@@ -94,74 +97,83 @@ export const Landing = styled.main`
 
 
 /*--------------Section Three----------------------*/
-.projectContainer{
-  height: 100%;
-  width: 90vw;
-  margin: 5% auto;
-}
+  .sectionThree{
+   height: 300px;
+  }
+  .projectContainer{
+    height: 100%;
+    width: 90vw;
+    margin: 5% auto 0;
+  }
 
-.projectContainer__title{
-  font-size: var(--font-size-large);
-  font-weight: var(--font-w-xlight);
-  border-bottom: 1px solid var(--palette-one-white);
-  margin: 0 40% 0 2vw;
-  height: 5%;
-}
-.title__right{
-  text-align: right;
-  margin: 0 2vw 0 40%;
-}
+  .projectContainer__title{
+    font-size: var(--font-size-large);
+    font-weight: var(--font-w-xlight);
+    border-bottom: 1px solid var(--palette-one-white);
+    margin: 0 40% 0 2vw;
+    height: 12%;
+  }
+  .title__right{
+    text-align: right;
+    margin: 0 2vw 0 40%;
+  }
 
-.projectContainer__list{
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+  .projectContainer__list{
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
 
-  list-style: none;
-  padding-inline: 0;
-  height: 25%;
-  margin-left: 2%;
-}
+    list-style: none;
+    padding-inline: 0;
+    margin-left: 2%;
+  }
+  .projectContainer__listItem{
+    width: 60%
+  }
+  .projectContainer__listItem:hover{
+    color: var(--palette-one-gray-l);
+    border-bottom: 1px dotted var(--palette-one-white);
+  }
 
 /*--------------Section Four----------------------*/
-.sectionFour{
-  padding-top: 30%;
-}
+  .sectionFour{
+    height: 600px;
+    padding-top: 15%;
+  }
 
-.contactList{
-  height: 40%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+  .contactList{
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
 
-  list-style: none;
-  padding-inline: 0;
+    list-style: none;
+    padding-inline: 0;
 
-  margin: 15% auto;
-  margin-left: 8%;
-}
+    margin: 15% auto;
+    margin-left: 8%;
+  }
 
-.contactList__item {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-.contactList__item img{
-  width: 48px;
-  margin-right: 5%;
-}
+  .contactList__item {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .contactList__item img{
+    width: 48px;
+    margin-right: 5%;
+  }
 
-.sectionFooter {
-  height: 20%;
-  width: 40%;
-  margin: auto;
-  margin-top: 8%;
-}
-
-
-  @media (min-width: 680px) {
+  .sectionFooter {
+    height: 20%;
+    width: 40%;
+    margin: auto;
+    margin-top: 8%;
+  }
 
 
+@media (min-width: 680px) {
     .sectionOne {
       height: 1120px;
     }
@@ -181,7 +193,7 @@ export const Landing = styled.main`
 }
 
  
-  @media (min-width: 1024px) {
+@media (min-width: 1024px) {
     .landingIntro {
       margin-top: 80px;
       padding: 0 0 0 55px;
@@ -192,5 +204,5 @@ export const Landing = styled.main`
       font-size: 38px;
       letter-spacing: 4px;
     }
-  }
+}
 `;
